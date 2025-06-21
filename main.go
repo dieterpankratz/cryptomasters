b6a5604eb1874ab72494466b6a9a45dd4db9005a
dpan <pankratzdieter@gmail.com>
2025-06-21 15:29:01 +0200

    async api calls with go routines

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"dpan/cryptomasters/api"
 	"fmt"
+	"sync"
 )
 
 func main() {
-	rate, err := api.GetRate("ETH")
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func() {
+			getRate(currency)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func getRate(currency string) {
+	rate, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("The rate for %v is %.2f \n", rate.Currency, rate.Price)
 	}
